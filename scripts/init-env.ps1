2f905ca08efbb1ccb1872d891cb02bbf6f19ddc7
Jone Aarre <jone.aarre@capgemini.com>
2024-01-02 11:35:45 +0100

    Add lint and publish jobs to Linux App Service template spec workflow

---
+++ scripts/init-env.ps1
@@ -0,0 +1,22 @@
+Connect-AzAccount
+
+$applicationRegistration = New-AzADApplication -DisplayName 'toy-reusable'
+New-AzADAppFederatedCredential `
+   -Name 'toy-reusable-branch' `
+   -ApplicationObjectId $applicationRegistration.Id `
+   -Issuer 'https://token.actions.githubusercontent.com' `
+   -Audience 'api://AzureADTokenExchange' `
+   -Subject "repo:$($githubOrganizationName)/$($githubRepositoryName):ref:refs/heads/main"
+
+$resourceGroup = New-AzResourceGroup -Name ToyReusable -Location norwayeast
+
+New-AzADServicePrincipal -AppId $applicationRegistration.AppId
+New-AzRoleAssignment `
+    -ApplicationId $applicationRegistration.AppId `
+    -RoleDefinitionName Contributor `
+    -Scope $resourceGroup.ResourceId
+
+$azureContext = Get-AzContext
+Write-Host "AZURE_CLIENT_ID: $($applicationRegistration.AppId)"
+Write-Host "AZURE_TENANT_ID: $($azureContext.Tenant.Id)"
+Write-Host "AZURE_SUBSCRIPTION_ID: $($azureContext.Subscription.Id)"
